68f132c76a4d5db4e36fbf26c834cbcd0ebbdc87
kimyou1102 <yoosk5485@naver.com>
2025-02-18 14:24:09 +0900

    test: 자동차 이름 입력 검증시 중복된 이름에 대한 케이스 추가

--- __tests__/inputValidation.test.js
+++ __tests__/inputValidation.test.js
@@ -1,7 +1,7 @@
 import { validateCarNames } from "../src/utils/validation.js";
 
 describe("예외 테스트", () => {
-  test.each(["", "01", "자동차."])(
+  test.each(["", "01", "자동차.", "데이지,머핀,데이지"])(
     `잘못된 자동차 이름 입력시 예외가 발생한다. (%s)`,
     (input) => {
       expect(() => validateCarNames(input)).toThrow("[ERROR]");
