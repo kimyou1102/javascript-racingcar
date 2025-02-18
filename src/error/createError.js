70b08ed4926601cb72ea5645ce71100245ba9dea
kimyou1102 <yoosk5485@naver.com>
2025-02-18 23:50:08 +0900

    chore: import문에 누락된 .js확장자 추가

--- src/error/createError.js
+++ src/error/createError.js
@@ -1,4 +1,4 @@
-import { ERROR_PREFIX } from "./message";
+import { ERROR_PREFIX } from "./message.js";
 
 export const createError = (message) => {
   throw new Error(`${ERROR_PREFIX} ${message}\n`);
