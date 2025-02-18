8d56d95d07d4ad195d20e62f9b3ed9b73c90bde0
kimyou1102 <yoosk5485@naver.com>
2025-02-19 00:10:18 +0900

    feat: Winners 클래스 생성자에 cars 파라미터 검증 로직 추가

--- src/domain/Winners.js
+++ src/domain/Winners.js
@@ -1,8 +1,27 @@
+import { createError } from "../error/createError.js";
+import { ERROR_MESSAGE } from "../error/message.js";
+
 export default class Winners {
   #cars;
 
   constructor(cars) {
     this.#cars = cars;
+
+    validateCars();
+  }
+
+  validateCars() {
+    if (Array.isArray(this.#cars)) {
+      createError(ERROR_MESSAGE.NOT_ARRAY);
+    }
+    this.#cars.forEach((car) => {
+      if (
+        typeof car.getName !== "function" ||
+        typeof car.getPosition !== "function"
+      ) {
+        createError(ERROR_MESSAGE.INVALID_CARS);
+      }
+    });
   }
 
   getWinners() {
--- src/error/message.js
+++ src/error/message.js
@@ -6,4 +6,6 @@ export const ERROR_MESSAGE = Object.freeze({
   INVALID_NAME_LENGTH: "자동차 이름의 길이는 1~5글자로 작성해주세요.",
   INVALID_TRY_COUNT: "시도 횟수를 1이상 숫자로 입력해주세요.",
   EMPTY_INPUT: "빈값은 입력할 수 없습니다.",
+  INVALID_CARS: "car 객체에 getName 메서드가 없거나 잘못 정의되었습니다.",
+  NOT_ARRAY: "배열이 아니거나 잘못 정의되었습니다.",
 });
