9b00baae01f5882e07e2bdd748a4fce22fca95d4
kimyou1102 <yoosk5485@naver.com>
2025-02-18 23:49:45 +0900

    refactor: 내장 메서드를 이용해서 선언형으로 변경

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,15 +1,14 @@
-import { OUTPUT_MESSAGE } from "./constants/message";
+import { OUTPUT_MESSAGE } from "./constants/message.js";
 
 export default class OutputView {
   printExecutionResult(results) {
     console.log(OUTPUT_MESSAGE.RESULT);
     results.forEach((result) => {
-      result.forEach((car) => {
-        console.log(
-          `${car.name} : ${OUTPUT_MESSAGE.PROGRESS_SYMBOL.repeat(car.position)}`
-        );
-      });
-      console.log("");
+      const resultStrings = result.map(
+        ({ name, position }) =>
+          `${name} : ${OUTPUT_MESSAGE.PROGRESS_SYMBOL.repeat(position)}\n`
+      );
+      console.log(resultStrings.join("\n"));
     });
   }
 
